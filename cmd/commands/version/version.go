8c9fd84e01f49e77a5ce41288985d86e11ce5a48
ranqiwu <857271370@qq.com>
2019-07-12 14:43:07 +0800

    version 1.12.7

--- cmd/commands/version/version.go
+++ cmd/commands/version/version.go
@@ -57,7 +57,7 @@ Prints the current Bee, Beego and Go version alongside the platform information.
 }
 var outputFormat string
 
-const version = "1.12.6"
+const version = "1.12.7"
 
 func init() {
 	fs := flag.NewFlagSet("version", flag.ContinueOnError)
